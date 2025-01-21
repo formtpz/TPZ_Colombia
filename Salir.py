678723e352b9f10634ab88e650da51ea2955e7dc
formtpz <137564298+formtpz@users.noreply.github.com>
2025-01-21 07:55:59 -0600

    Update Salir.py

--- Salir.py
+++ Salir.py
@@ -97,14 +97,6 @@ def Salir():
                         st.session_state.Salir=False
                         st.session_state.IFI=False
                         st.session_state.IFII=False
-                        st.session_state.IFIII=False
-                        st.session_state.Conformacion=False
-                        st.session_state.CC_Conformacion=False
-                        st.session_state.CC_IFI=False
-                        st.session_state.Auxiliares=False
-                        st.session_state.Validacion=False
-                        st.session_state.Testing=False
-                        st.session_state.Ubicacion=False
                         
                         puesto=pd.read_sql(f"select puesto from usuarios where usuario ='{usuario}'",uri)
                         puesto= puesto.loc[0,'puesto']
@@ -139,6 +131,6 @@ def Salir():
 
             st.header("Aplicación de uso exclusivo para el personal de Telespazio Argentina S.A.")
 
-            st.subheader("En caso de dudas favor revisar el siguiente video tutorial en el apartado 16. Registros y Bonos de la wiki")
+            st.subheader("En caso de dudas favor revisar el siguiente video tutorial )
 
-            st.subheader("Para soporte técnico favor escribir a daniela.rivera@telespazio.pe")
+            st.subheader("Para soporte técnico favor escribir a basilio.nunez@telespazio.com")
