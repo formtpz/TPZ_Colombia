28445192f9109f7d5d6c153dc5611e38a21f093e
formtpz <137564298+formtpz@users.noreply.github.com>
2025-01-27 10:50:19 -0600

    Update FMI.py

--- FMI.py
+++ FMI.py
@@ -222,9 +222,9 @@ def FMI(usuario,puesto):
 # df.astype('int64').dtypes
  
 # extracting the week from the date
-    semana_3 = fecha_3.df.week
+   # semana_3 = fecha_3.df.week
 
-   # semana_3 = datetime.date(fecha_3).isocalendar()[1]
+    semana_3 = datetime.fecha_3.isocalendar()[1]
 
     #semana_3 = fecha_3.strftime('%U')
  
