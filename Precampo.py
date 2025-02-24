22b9c4d73df5724107a23bb221db411873360201
formtpz <137564298+formtpz@users.noreply.github.com>
2025-02-24 11:51:08 -0600

    Update Precampo.py

--- Precampo.py
+++ Precampo.py
@@ -231,7 +231,7 @@ def Precampo(usuario,puesto):
 
     año_3 = fecha_3.isocalendar()[0]
 
-    unidad_3=municipio_3+'-'+consecutivo_3
+    unidad_3=municipio_3+'-'+str(consecutivo_3)
     
     cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Precampo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','0','0','{horas_3}')")
     con.commit()                                                                                                                                 
