795cdc4f3092e0f33ad8b172b1668392f73f11d7
formtpz <137564298+formtpz@users.noreply.github.com>
2025-02-26 16:39:20 -0600

    Update Bonos_Extras.py

--- Bonos_Extras.py
+++ Bonos_Extras.py
@@ -341,7 +341,7 @@ def Bonos_Extras(usuario,puesto):
       extras_9= pd.read_sql(f"select marca,usuario,nombre,puesto,supervisor,tipo_reporte,justificacion,fecha,horas,semana,dia,fecha_corte,fecha_bono from extras where tipo_reporte='Extra'  and fecha_bono='{periodo_9}'", con)
       extras_9=  pd.DataFrame(data=extras_9)
 
-      pivot4= len(extras _9.iloc[:,0])
+      pivot4= len(extras_9.iloc[:,0])
 
       if pivot4==0:
 
