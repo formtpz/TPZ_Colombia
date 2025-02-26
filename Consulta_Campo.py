45b1613f8b6a58060c498f6b7ca17ccd166b166f
formtpz <137564298+formtpz@users.noreply.github.com>
2025-02-26 16:18:46 -0600

    Update Consulta_Campo.py

--- Consulta_Campo.py
+++ Consulta_Campo.py
@@ -6,7 +6,7 @@ import psycopg2
 from datetime import datetime
 import pytz
 from urllib.parse import urlparse
-import Procesos,Historial,Capacitacion,Otros_Registros,Bonos,Salir
+import Procesos,Historial,Capacitacion,Otros_Registros,Bonos_Extras,Salir
 
 def Consulta_Campo(usuario,puesto):
 
@@ -37,7 +37,7 @@ def Consulta_Campo(usuario,puesto):
   otros_registros_3 = placeholder5_3.button("Otros Registros",key="otros_registros_3")
 
   placeholder6_3 = st.sidebar.empty()
-  bonos_3 = placeholder6_3.button("Bonos",key="bonos_3")
+  bonos_extras_3 = placeholder6_3.button("Bonos y Horas Extra",key="bonos_extras_3")
 
   placeholder7_3 = st.sidebar.empty()
   salir_3 = placeholder7_3.button("Salir",key="salir_3")
@@ -150,9 +150,9 @@ def Consulta_Campo(usuario,puesto):
     st.session_state.Otros_Registros=True
     Otros_Registros.Otros_Registros(usuario,puesto)
 
-  # ----- Bonos ---- #
+  # ----- Bonos y Horas Extra---- #
     
-  elif bonos_3:
+  elif bonos_extras_3:
     placeholder1_3.empty()
     placeholder2_3.empty()
     placeholder3_3.empty()
@@ -166,8 +166,8 @@ def Consulta_Campo(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     st.session_state.Consulta_Campo=False
-    st.session_state.Bonos=True
-    Bonos.Bonos(usuario,puesto)    
+    st.session_state.Bonos_Extras=True
+    Bonos_Extras.Bonos_Extras(usuario,puesto)    
 
     # ----- Salir ---- #
     
