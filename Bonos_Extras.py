3a15f85479b4d033bfb2b5b0ef54ae78d4cfa084
formtpz <137564298+formtpz@users.noreply.github.com>
2025-02-28 09:18:13 -0600

    Update Bonos_Extras.py

--- Bonos_Extras.py
+++ Bonos_Extras.py
@@ -92,7 +92,7 @@ def Bonos_Extras(usuario,puesto):
   elif nombre_9=="Gabriel Martin Prieto" or nombre_9=="Madeline Hernandez Gamboa":
 
     data_personal_9 = pd.read_sql(f"select nombre from usuarios where estado='Activo'", con)
-    Todos=["Todos"]
+    Todos=['Todos']
     data_personal_9.append(Todos)
     placeholder13_9 = st.empty()
     personal_9= placeholder13_9.selectbox("Personal",data_personal_9,key="personal_9")
