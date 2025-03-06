e2fe7b16545f49e6c93a365830429e67f7d16d17
formtpz <137564298+formtpz@users.noreply.github.com>
2025-03-06 16:09:53 -0600

    Update CC_FMI.py

--- CC_FMI.py
+++ CC_FMI.py
@@ -234,6 +234,8 @@ def CC_FMI(usuario,puesto):
 
     año_3 = fecha_3.isocalendar()[0]
 
+    unidad_3=municipio_3+'-'+unidad_inter_3
+    
     cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Control de Calidad Folios de Matricula Inmobiliaria','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','{aprobados_3}','{rechazados_3}','0')")
     con.commit()                                                                                                                                 
     st.success('Reporte enviado correctamente')
