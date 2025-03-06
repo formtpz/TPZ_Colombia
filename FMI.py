02a9ece19ffa4d6f5b8331f97021ded9b7e9de22
formtpz <137564298+formtpz@users.noreply.github.com>
2025-03-06 16:05:50 -0600

    Update FMI.py

--- FMI.py
+++ FMI.py
@@ -52,16 +52,19 @@ def FMI(usuario,puesto):
   fecha_3= placeholder9_3.date_input("Fecha",value=default_date_3,key="fecha_3")
 
   placeholder10_3= st.empty()
-  unidad_3=placeholder10_3.selectbox("Unidad de Asignación", options=("Cabuyaro – Urbano","Chalán – Urbano","Colombia – Urbano","Cuítiva – Urbano","Iza – Urbano","Los Palmitos – Urbano","Morroa – Urbano","Trinidad – Urbano","San Estanislao – Urbano","San Luis de Cubarral – Urbano","UITU- 1","UITU- 2","UITU- 3","UITU- 4","UITU- 5","UITU- 6","UITU- 7","Zambrano – Urbano"), key="unidad_3")
-
+  municipio_3= placeholder10_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
+  
   placeholder11_3= st.empty()
-  tipo_3= placeholder11_3.selectbox("Tipo", options=("Ordinario","Corrección"), key="tipo_3")
-                  
-  placeholder12_3= st.empty()
-  produccion_3= placeholder12_3.number_input("Cantidad de Folios Revisados",min_value=0,step=1,key="produccion_3")
+  unidad_inter_3=placeholder11_3.selectbox("Unidad de Intervención", options=("UITR-1","UITR-2","UITR-3","UITR-4","UITR-5","UITR-6","UITR-7","UITR-8","UITR-9","UITR-10","UITR-11","UITR-12","UITR-13","UITR-14","UITR-15","UITR-16","UITR-17","UITR-18","UITR-19","UITR-20","UITU-1","UIU-2","UITU-3","UITU-4","UITU-5","UITU-6","UITU-7","UITU-8","UITU-9","UITU-10","UITU-11","UITU-12","UITU-13","UITU-14","UITU-15","UTU-16","UITU-17","UITU- 18","UITU-19","UITU-20"), key="unidad_inter_3")
 
-  placeholder13_3 = st.empty()
-  reporte_3 = placeholder13_3.button("Generar Reporte",key="reporte_3")
+  placeholder12_3= st.empty()
+  tipo_3= placeholder12_3.selectbox("Tipo", options=("Ordinario","Corrección"), key="tipo_3")
+                  
+  placeholder13_3= st.empty()
+  produccion_3= placeholder13_3.number_input("Cantidad de Folios Revisados",min_value=0,step=1,key="produccion_3")
+
+  placeholder14_3 = st.empty()
+  reporte_3 = placeholder14_3.button("Generar Reporte",key="reporte_3")
 
   # ----- Procesos ---- #
     
@@ -79,6 +82,7 @@ def FMI(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
+    placeholder14_3.empty()
     st.session_state.Procesos=False
     st.session_state.FMI=False
 
@@ -114,6 +118,7 @@ def FMI(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
+    placeholder14_3.empty()
     st.session_state.FMI=False
     st.session_state.Historial=True
     Historial.Historial(usuario,puesto)   
@@ -134,6 +139,7 @@ def FMI(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
+    placeholder14_3.empty()
     st.session_state.FMI=False
     st.session_state.Capacitacion=True
     Capacitacion.Capacitacion(usuario,puesto)
@@ -154,6 +160,7 @@ def FMI(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
+    placeholder14_3.empty()
     st.session_state.FMI=False
     st.session_state.Otros_Registros=True
     Otros_Registros.Otros_Registros(usuario,puesto)
@@ -174,6 +181,7 @@ def FMI(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
+    placeholder14_3.empty()
     st.session_state.FMI=False
     st.session_state.Bonos_Extras=True
     Bonos_Extras.Bonos_Extras(usuario,puesto)    
@@ -194,6 +202,7 @@ def FMI(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
+    placeholder14_3.empty()
     st.session_state.Ingreso = False
     st.session_state.FMI=False
     st.session_state.Salir=True
@@ -215,7 +224,7 @@ def FMI(usuario,puesto):
 
     año_3 = fecha_3.isocalendar()[0]
 
-    unidad_3=municipio_3+'-'+str(consecutivo_3)
+    unidad_3=municipio_3+'-'+unidad_inter_3
 
     cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Folios de Matricula Inmobiliaria','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','0','0','0')")
     con.commit()                                                                                                                                 
