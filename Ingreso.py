16a4a859300d4c6c68c2b30792b593ada9eab2b3
formtpz <137564298+formtpz@users.noreply.github.com>
2025-03-18 17:34:39 -0600

    Update Ingreso.py

--- Ingreso.py
+++ Ingreso.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Formularios TPZ",page_icon=img,layout="wide")
 
 footer = """
 <style>
-.footer {
+.footer:after {
     position: relative;
     # width: 100%;
     display:block;
