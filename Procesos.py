0ab52b147bcb090aa8f77bd4a163aa6d01ffce74
formtpz <137564298+formtpz@users.noreply.github.com>
2025-03-18 11:58:34 -0600

    Update Procesos.py

--- Procesos.py
+++ Procesos.py
@@ -315,7 +315,7 @@ def Procesos1(usuario,puesto):
             placeholder14_2.empty()
             st.session_state.Procesos=True
             st.session_state.CC_Validacion=True
-            CC_Validacion.CC_Validacion(usuario,puesto)
+             CC_Validacion.CC_Validacion(usuario,puesto)
     
     elif st.session_state.Procesos==True:
 
