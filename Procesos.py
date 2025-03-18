98169c3b580f008dc42e6823200c9e998cc7062a
formtpz <137564298+formtpz@users.noreply.github.com>
2025-03-18 13:55:50 -0600

    Update Procesos.py

--- Procesos.py
+++ Procesos.py
@@ -1,7 +1,7 @@
 # ----- Librerías ---- #
 
 import streamlit as st
-import Historial, Capacitacion, Otros_Registros, Bonos_Extras, Salir, FMI, CC_FMI, Consulta_Campo, Precampo, CC_Precampo, Validacion, Prueba
+import Historial, Capacitacion, Otros_Registros, Bonos_Extras, Salir, FMI, CC_FMI, Consulta_Campo, Precampo, CC_Precampo,Prueba
 # ----- Procesos 1 ---- #
 
 def Procesos1(usuario,puesto):
