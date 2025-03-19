c2f65f101c7e3214ba5dd5acc37e2aa111c249b9
formtpz <137564298+formtpz@users.noreply.github.com>
2025-03-19 16:36:07 -0600

    Update Ingreso.py

--- Ingreso.py
+++ Ingreso.py
@@ -44,11 +44,11 @@ hide_streamlit_style = """
                 }
                 footer {
                 visibility: visible;
-                content:'V.1.2 Copyrigth @ 2025 Telespazio Argentina S.A.';
-                display: block;
-                position: relative;
-                color: tomato;
-                height: 0%;
+                  content:'V.1.2 Copyrigth @ 2025 Telespazio Argentina S.A.';
+                  display: block;
+                  position: relative;
+                  color: tomato;
+                  height: 0%;
                 }
                 </style>
                 """
