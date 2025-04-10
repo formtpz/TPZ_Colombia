0b6be6bcb7371d267b6c289e8aa6f589cb07e64a
formtpz <137564298+formtpz@users.noreply.github.com>
2025-04-10 12:21:41 -0600

    Update Ingreso.py

--- Ingreso.py
+++ Ingreso.py
@@ -133,12 +133,13 @@ if iniciar_sesion_1:
                 st.session_state.FMI=False
                 st.session_state.CC_FMI=False
                 st.session_state.Consulta_Campo=False
-                st.session_state.Precampo=False
+                st.session_state.Precampo=False 
                 st.session_state.CC_Precampo=False
+                st.session_state.Preparacion_Insumos=False
+                st.session_state.Revision_Campo=False
                 st.session_state.Validacion=False
                 st.session_state.CC_Validacion=False
-                st.session_state.Revision_Campo=False
-                    
+                                    
                 puesto=pd.read_sql(f"select puesto from usuarios where usuario ='{usuario}'",uri)
                 puesto= puesto.loc[0,'puesto']
                    
