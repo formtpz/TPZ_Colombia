d509468100eabfa27afdfae1f7b5438110cdb146
formtpz <137564298+formtpz@users.noreply.github.com>
2025-04-10 12:22:58 -0600

    Update Salir.py

--- Salir.py
+++ Salir.py
@@ -100,10 +100,11 @@ def Salir():
                         st.session_state.Consulta_Campo=False
                         st.session_state.Precampo=False
                         st.session_state.CC_Precampo=False
+                        st.session_state.Preparacion_Insumos=False
+                        st.session_state.Revision_Campo=False
                         st.session_state.Validacion=False
                         st.session_state.CC_Validacion=False
-                        st.session_state.Revision_Campo=False
-                        
+                                                
                         puesto=pd.read_sql(f"select puesto from usuarios where usuario ='{usuario}'",uri)
                         puesto= puesto.loc[0,'puesto']
 
