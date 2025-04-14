40d1fca52189b7d879c1bc491e4252497cb5ba06
formtpz <137564298+formtpz@users.noreply.github.com>
2025-04-14 13:47:31 -0600

    Update CC_Validacion.py

--- CC_Validacion.py
+++ CC_Validacion.py
@@ -57,7 +57,7 @@ def CC_Validacion(usuario,puesto):
   consecutivo_3= placeholder11_3.number_input("Número de Paquete",min_value=0,max_value=600, step=1, key="consecutivo_3")
 
   placeholder12_3= st.empty()
-  tipo_3= placeholder12_3.selectbox("Tipo", options=("Ordinario","Reinspección"), key="tipo_3")
+  tipo_3= placeholder12_3.selectbox("Tipo", options=("Inspección","Primera Reinspección","Segunda Reinspección"), key="tipo_3")
   
   placeholder13_3= st.empty()
   aprobados_3= placeholder13_3.number_input("Cantidad de Predios Aprobados",min_value=0,step=1,key="aprobados_3")
