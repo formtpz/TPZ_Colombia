839bb1b2e6c7752074b72182c0f45d9d21241a10
formtpz <137564298+formtpz@users.noreply.github.com>
2025-05-01 20:53:44 -0600

    Update Bonos_Extras.py

--- Bonos_Extras.py
+++ Bonos_Extras.py
@@ -495,7 +495,7 @@ def Bonos_Extras(usuario,puesto):
       
       else:
   
-        bloques_9= pd.read_sql(f"select nombre,supervisor,proceso,unidad_asignacion,tipo_revision,produccion_segun_reporte,produccion_rechazada_primera_revision,produccion_aprobada_primera_revision,porcentage_error,produccion_penalizada,produccion_limpia,fecha_produccion,fecha_bono from unidades where nombre='{usuario}' and fecha_produccion='{periodo_bloques_9}'", con)
+        bloques_9= pd.read_sql(f"select nombre,supervisor,proceso,unidad_asignacion,tipo_revision,produccion_segun_reporte,produccion_rechazada_primera_revision,produccion_aprobada_primera_revision,porcentage_error,produccion_penalizada,produccion_limpia,fecha_produccion,fecha_bono from unidades where nombre='{personal_9}' and fecha_produccion='{periodo_bloques_9}'", con)
         bloques_9=  pd.DataFrame(data=bloques_9)
       
   
