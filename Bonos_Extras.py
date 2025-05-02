3470cb7294aa9caee22a34b96eb87f99d362a1fd
formtpz <137564298+formtpz@users.noreply.github.com>
2025-05-01 20:43:14 -0600

    Update Bonos_Extras.py

--- Bonos_Extras.py
+++ Bonos_Extras.py
@@ -387,7 +387,7 @@ def Bonos_Extras(usuario,puesto):
   
   elif nombre_9=="Ignacio Aguglino":
 
-    data_personal_9 = pd.read_sql(f"select nombre from usuarios where estado='Activo'", con)
+    data_personal_9 = pd.read_sql(f"select nombre from usuarios where 'Profesional Jurídico' and estado='Activo'", con)
     Todo = pd.DataFrame({"nombre": ["Todos"]})
     data_personal_9 = pd.concat([data_personal_9,Todo],ignore_index=True)
 
