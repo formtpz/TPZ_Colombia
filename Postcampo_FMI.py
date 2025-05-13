12c15dcc9d4b65651ba3caafebc6ad58ae240af1
formtpz <137564298+formtpz@users.noreply.github.com>
2025-05-13 14:02:11 -0600

    Update Postcampo_FMI.py

--- Postcampo_FMI.py
+++ Postcampo_FMI.py
@@ -44,7 +44,7 @@ def Postcampo_FMI(usuario,puesto):
   salir_3 = placeholder7_3.button("Salir",key="salir_3")
 
   placeholder8_3 = st.empty()
-  fmi_3 = placeholder8_3.title("Postcampo Folios de Matricula Inmobiliaria")
+  postcampo_fmi_3 = placeholder8_3.title("Postcampo Folios de Matricula Inmobiliaria")
 
   default_date_3 = datetime.now(pytz.timezone('America/Bogota'))
 
