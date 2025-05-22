3435a5948b071c21de2a55f9c56d1b094e466227
formtpz <137564298+formtpz@users.noreply.github.com>
2025-05-22 17:06:18 -0600

    Update Precampo.py

--- Precampo.py
+++ Precampo.py
@@ -233,6 +233,6 @@ def Precampo(usuario,puesto):
 
     unidad_3=municipio_3+'-'+str(consecutivo_3)
     
-    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Precampo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','0','0','{horas_3}')")
+    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Precampo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','0','0','{horas_3}','0','0','0','0')")
     con.commit()                                                                                                                                 
     st.success('Reporte enviado correctamente')
