fa39e2ba2b16d7296f64b0330cef8475bb75411b
formtpz <137564298+formtpz@users.noreply.github.com>
2025-05-23 13:35:21 -0600

    Update FMI.py

--- FMI.py
+++ FMI.py
@@ -50,21 +50,33 @@ def FMI(usuario,puesto):
 
   placeholder9_3= st.empty()
   fecha_3= placeholder9_3.date_input("Fecha",value=default_date_3,key="fecha_3")
-
+  
   placeholder10_3= st.empty()
-  municipio_3= placeholder10_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
+  lote_3= placeholder10_3.selectbox("Lote", options=("1","2","3"), key="lote_3")
   
   placeholder11_3= st.empty()
-  unidad_inter_3=placeholder11_3.selectbox("Unidad de Intervención", options=("UITR-1","UITR-2","UITR-3","UITR-4","UITR-5","UITR-6","UITR-7","UITR-8","UITR-9","UITR-10","UITR-11","UITR-12","UITR-13","UITR-14","UITR-15","UITR-16","UITR-17","UITR-18","UITR-19","UITR-20","UITR-21","UITR-22","UITR-23","UITR-24","UITR-25","UITR-26","UITR-27","UITR-28","UITR-29","UITR-30","UITR-31","UITR-32","UITR-33","UITR-34","UITR-35","UITR-36","UITR-37","UITR-38","UITR-39","UITR-40","UITU-1","UITU-2","UITU-3","UITU-4","UITU-5","UITU-6","UITU-7","UITU-8","UITU-9","UITU-10","UITU-11","UITU-12","UITU-13","UITU-14","UITU-15","UITU-16","UITU-17","UITU-18","UITU-19","UITU-20","UITU-21","UITU-22","UITU-23","UITU-24","UITU-25","UITU-25","UITU-26","UITU-27","UITU-28","UITU-29","UITU-30","UITU-31","UITU-32","UITU-33","UITU-34","UITU-35","UITU-36","UITU-37","UITU-38","UITU-39","UITU-40","Sin Geometría"), key="unidad_inter_3")
-
+  municipio_3= placeholder11_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
+  
   placeholder12_3= st.empty()
-  tipo_3= placeholder12_3.selectbox("Tipo", options=("Ordinario","Corrección"), key="tipo_3")
-                  
+  unidad_inter_3=placeholder12_3.selectbox("Unidad de Intervención", options=("UITR-1","UITR-2","UITR-3","UITR-4","UITR-5","UITR-6","UITR-7","UITR-8","UITR-9","UITR-10","UITR-11","UITR-12","UITR-13","UITR-14","UITR-15","UITR-16","UITR-17","UITR-18","UITR-19","UITR-20","UITR-21","UITR-22","UITR-23","UITR-24","UITR-25","UITR-26","UITR-27","UITR-28","UITR-29","UITR-30","UITR-31","UITR-32","UITR-33","UITR-34","UITR-35","UITR-36","UITR-37","UITR-38","UITR-39","UITR-40","UITU-1","UITU-2","UITU-3","UITU-4","UITU-5","UITU-6","UITU-7","UITU-8","UITU-9","UITU-10","UITU-11","UITU-12","UITU-13","UITU-14","UITU-15","UITU-16","UITU-17","UITU-18","UITU-19","UITU-20","UITU-21","UITU-22","UITU-23","UITU-24","UITU-25","UITU-25","UITU-26","UITU-27","UITU-28","UITU-29","UITU-30","UITU-31","UITU-32","UITU-33","UITU-34","UITU-35","UITU-36","UITU-37","UITU-38","UITU-39","UITU-40","Sin Geometría"), key="unidad_inter_3")
+  
   placeholder13_3= st.empty()
-  produccion_3= placeholder13_3.number_input("Cantidad de Folios Revisados",min_value=0,step=1,key="produccion_3")
+  estado_3= placeholder13_3.selectbox("Estado", options=("Revisión de Folios","Control de Calidad Precampo","Predios sin Geometría","Devuelto a Campo", "Control de Calidad Post Campo", "Control de Calidad Previo XTF (Externo)"), key="estado_3")
+  
+  placeholder14_3= st.empty()
+  tipo_3= placeholder14_3.selectbox("Tipo", options=("Ordinario","Corrección"), key="tipo_3")
+                  
+  placeholder15_3= st.empty()
+  produccion_3= placeholder15_3.number_input("Cantidad de Folios Revisados",min_value=0,step=1,key="produccion_3")
 
-  placeholder14_3 = st.empty()
-  reporte_3 = placeholder14_3.button("Generar Reporte",key="reporte_3")
+  placeholder16_3= st.empty()
+  folios_con_fmi_3= placeholder16_3.number_input("Cantidad de Folios con FMI",min_value=0,step=1,key="folios_con_fmi_3")
+
+  placeholder17_3= st.empty()
+  predios_sin_fmi_3= placeholder17_3.number_input("Cantidad de Predios sin FMI",min_value=0,step=1,key="predios_sin_fmi_3")
+
+  placeholder18_3 = st.empty()
+  reporte_3 = placeholder18_3.button("Generar Reporte",key="reporte_3")
 
   # ----- Procesos ---- #
     
@@ -83,6 +95,10 @@ def FMI(usuario,puesto):
     placeholder12_3.empty()
     placeholder13_3.empty()
     placeholder14_3.empty()
+    placeholder15_3.empty()
+    placeholder16_3.empty()
+    placeholder17_3.empty()
+    placeholder18_3.empty()
     st.session_state.Procesos=False
     st.session_state.FMI=False
 
@@ -119,6 +135,10 @@ def FMI(usuario,puesto):
     placeholder12_3.empty()
     placeholder13_3.empty()
     placeholder14_3.empty()
+    placeholder15_3.empty()
+    placeholder16_3.empty()
+    placeholder17_3.empty()
+    placeholder18_3.empty()
     st.session_state.FMI=False
     st.session_state.Historial=True
     Historial.Historial(usuario,puesto)   
@@ -140,6 +160,10 @@ def FMI(usuario,puesto):
     placeholder12_3.empty()
     placeholder13_3.empty()
     placeholder14_3.empty()
+    placeholder15_3.empty()
+    placeholder16_3.empty()
+    placeholder17_3.empty()
+    placeholder18_3.empty()
     st.session_state.FMI=False
     st.session_state.Capacitacion=True
     Capacitacion.Capacitacion(usuario,puesto)
@@ -161,6 +185,10 @@ def FMI(usuario,puesto):
     placeholder12_3.empty()
     placeholder13_3.empty()
     placeholder14_3.empty()
+    placeholder15_3.empty()
+    placeholder16_3.empty()
+    placeholder17_3.empty()
+    placeholder18_3.empty()
     st.session_state.FMI=False
     st.session_state.Otros_Registros=True
     Otros_Registros.Otros_Registros(usuario,puesto)
@@ -182,6 +210,10 @@ def FMI(usuario,puesto):
     placeholder12_3.empty()
     placeholder13_3.empty()
     placeholder14_3.empty()
+    placeholder15_3.empty()
+    placeholder16_3.empty()
+    placeholder17_3.empty()
+    placeholder18_3.empty()
     st.session_state.FMI=False
     st.session_state.Bonos_Extras=True
     Bonos_Extras.Bonos_Extras(usuario,puesto)    
@@ -203,6 +235,10 @@ def FMI(usuario,puesto):
     placeholder12_3.empty()
     placeholder13_3.empty()
     placeholder14_3.empty()
+    placeholder15_3.empty()
+    placeholder16_3.empty()
+    placeholder17_3.empty()
+    placeholder18_3.empty()
     st.session_state.Ingreso = False
     st.session_state.FMI=False
     st.session_state.Salir=True
@@ -226,6 +262,6 @@ def FMI(usuario,puesto):
 
     unidad_3=municipio_3+'-'+unidad_inter_3
 
-    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Folios de Matricula Inmobiliaria','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','0','0','0')")
+    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Folios de Matricula Inmobiliaria','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','0','0','0','{uit_3}','0','{lote_3}','{estado_3}','0','0','0','0','{folios_con_fmi_3},'{predios_sin_fmi_3}')")
     con.commit()                                                                                                                                 
     st.success('Reporte enviado correctamente')
