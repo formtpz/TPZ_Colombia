c49535e3cecbffb03ce0fc5eaf668c963006a20f
formtpz <137564298+formtpz@users.noreply.github.com>
2025-06-03 14:57:22 -0600

    Update CC_Validacion.py

--- CC_Validacion.py
+++ CC_Validacion.py
@@ -242,7 +242,7 @@ def CC_Validacion(usuario,puesto):
 
     año_3 = fecha_3.isocalendar()[0]
 
-    unidad_3=municipio_3+'-'+'Paquete'+'-'+str(consecutivo_3)
+    unidad_3=municipio_3
 
     cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi,observaciones)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Control de Calidad Postcampo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','{aprobados_3}','{rechazados_3}','{horas_3}','0','0','0','0','0','0','0','{operador_3}','0','0','0')")
     con.commit()                                                                                                                                 
