2fa3b5c08fa3798c0339b39d131722b01353cc0a
formtpz <137564298+formtpz@users.noreply.github.com>
2025-06-25 12:28:40 -0600

    Update CC_Validacion.py

--- CC_Validacion.py
+++ CC_Validacion.py
@@ -261,7 +261,9 @@ def CC_Validacion(usuario,puesto):
     año_3 = fecha_3.isocalendar()[0]
 
     unidad_3=municipio_3
-
-    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi,observaciones,zona)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Control de Calidad Postcampo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','{aprobados_3}','{rechazados_3}','{horas_3}','UIT-0','0','0','N/A','0.0','0','0','{operador_3}','0','0','N/A','{zona_3}','{tipo_calidad_3}')")
+    horas_bi = float(horas_3)
+    area_bi = float(area_3)
+    
+    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Control de Calidad Postcampo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','{aprobados_3}','{rechazados_3}','{horas_3}','UIT-0','0','0','N/A','0.0','0','0','{operador_3}','0','0','N/A','{zona_3}','{tipo_calidad_3}','{horas_bi}','{area_bi}')")
     con.commit()                                                                                                                                 
     st.success('Reporte enviado correctamente')
