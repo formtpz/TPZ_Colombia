8cdc35c23de546f8f42ca7a977654cbddb74e8a2
formtpz <137564298+formtpz@users.noreply.github.com>
2025-07-03 09:52:35 -0600

    Update Precampo.py

--- Precampo.py
+++ Precampo.py
@@ -49,51 +49,48 @@ def Precampo(usuario,puesto):
 
   placeholder9_3= st.empty()
   fecha_3= placeholder9_3.date_input("Fecha",value=default_date_3,key="fecha_3")
-
+  
   placeholder10_3= st.empty()
-  lote_3= placeholder10_3.selectbox("Lote", options=("1","2","3"), key="lote_3")
+  municipio_3= placeholder10_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
   
   placeholder11_3= st.empty()
-  municipio_3= placeholder11_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
-  
+  hito_3= placeholder11_3.selectbox("Hito", options=("1","2","3","4","5","6","7","8","9","10"), key="hito_3")
+
   placeholder12_3= st.empty()
-  hito_3= placeholder12_3.selectbox("Hito", options=("1","2","3","4","5","6","7","8","9","10"), key="hito_3")
+  uit_3= placeholder12_3.text_input("UIT, Siga siempre el siguiente formato: UIT-1 RURAL (Que las letras siempre vayan en Mayúsculas).", max_chars=60, key="uit_3")
 
   placeholder13_3= st.empty()
-  uit_3= placeholder13_3.text_input("UIT, Siga siempre el siguiente formato: UIT-1 RURAL (Que las letras siempre vayan en Mayúsculas).", max_chars=60, key="uit_3")
-
+  zona_3= placeholder13_3.selectbox("Zona",options=("Urbano","Rural","Sin Geometría"), key="zona_3")
+  
   placeholder14_3= st.empty()
-  zona_3= placeholder14_3.selectbox("Zona",options=("Urbano","Rural","Sin Geometría"), key="zona_3")
+  consecutivo_3= placeholder14_3.text_input("Consecutivo (Unidad de Asignación)" ,max_chars=3,key="consecutivo_3")
   
   placeholder15_3= st.empty()
-  consecutivo_3= placeholder15_3.text_input("Consecutivo (Unidad de Asignación)" ,max_chars=3,key="consecutivo_3")
+  tipo_3= placeholder15_3.selectbox("Tipo", options=("Ordinario","Corrección de Calidad","Corrección Inspección","Corrección Primera Reinspección","Reproceso Ordinario","Reproceso Corrección Inspección","Reproceso Corrección Primera Reinspección"), key="tipo_3")
   
   placeholder16_3= st.empty()
-  tipo_3= placeholder16_3.selectbox("Tipo", options=("Ordinario","Corrección de Calidad","Corrección Inspección","Corrección Primera Reinspección","Reproceso Ordinario","Reproceso Corrección Inspección","Reproceso Corrección Primera Reinspección"), key="tipo_3")
+  estado_3= placeholder16_3.selectbox("Estado" , options=("En Proceso", "Finalizado"),key="estado_3")
   
   placeholder17_3= st.empty()
-  estado_3= placeholder17_3.selectbox("Estado" , options=("En Proceso", "Finalizado"),key="estado_3")
-  
+  area_3= placeholder17_3.number_input("Area en m²",min_value=0.0,key="area_3")
+
   placeholder18_3= st.empty()
-  area_3= placeholder18_3.number_input("Area en m²",min_value=0.0,key="area_3")
+  produccion_3= placeholder18_3.number_input("Cantidad Total de Predios Producidos",min_value=0,key="produccion_3")
 
   placeholder19_3= st.empty()
-  produccion_3= placeholder19_3.number_input("Cantidad Total de Predios Producidos",min_value=0,key="produccion_3")
-
+  informales_3= placeholder19_3.number_input("Cantidad de Predios Informales",min_value=0,key="informales_3")
+  
   placeholder20_3= st.empty()
-  informales_3= placeholder20_3.number_input("Cantidad de Predios Informales",min_value=0,key="informales_3")
+  efes_3= placeholder20_3.number_input("Cantidad de F",min_value=0,key="efes_3")
   
   placeholder21_3= st.empty()
-  efes_3= placeholder21_3.number_input("Cantidad de F",min_value=0,key="efes_3")
-  
+  horas_3= placeholder21_3.number_input("Cantidad de Horas Trabajadas en el Proceso",min_value=0.0,key="horas_3")
+
   placeholder22_3= st.empty()
-  horas_3= placeholder22_3.number_input("Cantidad de Horas Trabajadas en el Proceso",min_value=0.0,key="horas_3")
+  observaciones_3= placeholder22_3.text_input("Observaciones", max_chars=60, key="observaciones_3")
 
-  placeholder23_3= st.empty()
-  observaciones_3= placeholder23_3.text_input("Observaciones", max_chars=60, key="observaciones_3")
-
-  placeholder24_3 = st.empty()
-  reporte_3 = placeholder24_3.button("Generar Reporte",key="reporte_3")
+  placeholder23_3 = st.empty()
+  reporte_3 = placeholder23_3.button("Generar Reporte",key="reporte_3")
 
   # ----- Procesos ---- #
     
@@ -121,7 +118,6 @@ def Precampo(usuario,puesto):
     placeholder21_3.empty()
     placeholder22_3.empty()
     placeholder23_3.empty()
-    placeholder24_3.empty()
     st.session_state.Procesos=False
     st.session_state.Precampo=False
 
@@ -166,7 +162,6 @@ def Precampo(usuario,puesto):
     placeholder21_3.empty()
     placeholder22_3.empty()
     placeholder23_3.empty()
-    placeholder24_3.empty()
     st.session_state.Precampo=False
     st.session_state.Historial=True
     Historial.Historial(usuario,puesto)   
@@ -197,7 +192,6 @@ def Precampo(usuario,puesto):
     placeholder21_3.empty()
     placeholder22_3.empty()
     placeholder23_3.empty()
-    placeholder24_3.empty()
     st.session_state.Precampo=False
     st.session_state.Capacitacion=True
     Capacitacion.Capacitacion(usuario,puesto)
@@ -228,7 +222,6 @@ def Precampo(usuario,puesto):
     placeholder21_3.empty()
     placeholder22_3.empty()
     placeholder23_3.empty()
-    placeholder24_3.empty()
     st.session_state.Precampo=False
     st.session_state.Otros_Registros=True
     Otros_Registros.Otros_Registros(usuario,puesto)
@@ -259,7 +252,6 @@ def Precampo(usuario,puesto):
     placeholder21_3.empty()
     placeholder22_3.empty()
     placeholder23_3.empty()
-    placeholder24_3.empty()
     st.session_state.Precampo=False
     st.session_state.Bonos_Extras=True
     Bonos_Extras.Bonos_Extras(usuario,puesto)    
@@ -290,7 +282,6 @@ def Precampo(usuario,puesto):
     placeholder21_3.empty()
     placeholder22_3.empty()
     placeholder23_3.empty()
-    placeholder24_3.empty()
     st.session_state.Ingreso = False
     st.session_state.Precampo=False
     st.session_state.Salir=True
@@ -314,6 +305,19 @@ def Precampo(usuario,puesto):
 
     unidad_3=municipio_3+'-'+str(consecutivo_3)
 
+      # ----- Almacenar Lote_3 según municipio seleccionado ---- #
+    
+    lote_3_municipios = {"Cabuyaro", "Colombia", "Cubarral"}
+    lote_2_municipios = {"Trinidad", "Iza", "Cuítiva"}
+   
+    if municipio_3 in lote_3_municipios:
+      lote_3 = '3'
+    elif municipio_3 in lote_2_municipios:
+      lote_3 = '2'
+    else:
+      lote_3 = '1'
+      # ----- Fin del script ---- #
+    
     horas_bi = float(horas_3)
     area_bi = float(area_3)
     
