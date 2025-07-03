6a316a2fff2683b3cd21aa6a5905d0d243777e5e
formtpz <137564298+formtpz@users.noreply.github.com>
2025-07-03 10:05:43 -0600

    Update Precampo.py

--- Precampo.py
+++ Precampo.py
@@ -51,7 +51,7 @@ def Precampo(usuario,puesto):
   fecha_3= placeholder9_3.date_input("Fecha",value=default_date_3,key="fecha_3")
   
   placeholder10_3= st.empty()
-  municipio_3= placeholder10_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
+  municipio_3= placeholder10_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","Cubarral","Zambrano"), key="municipio_3")
   
   placeholder11_3= st.empty()
   hito_3= placeholder11_3.selectbox("Hito", options=("1","2","3","4","5","6","7","8","9","10"), key="hito_3")
