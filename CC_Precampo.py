bee924271be9181b5458c19d10b56ed10eb1869a
formtpz <137564298+formtpz@users.noreply.github.com>
2025-07-14 15:30:26 -0600

    Update CC_Precampo.py

--- CC_Precampo.py
+++ CC_Precampo.py
@@ -265,7 +265,7 @@ def CC_Precampo(usuario,puesto):
     horas_bi = float(horas_3)
       # ----- Almacenar Lote_3 según municipio seleccionado ---- #
     
-    lote_3_municipios = {"Cabuyaro", "Colombia", "Cubarral"}
+    lote_3_municipios = {"Cabuyaro", "Colombia", "San Luis de Cubarral"}
     lote_2_municipios = {"Trinidad", "Iza", "Cuítiva"}
    
     if municipio_3 in lote_3_municipios:
