86189203298e2a313f4b76a7cb70fcef83431f42
formtpz <137564298+formtpz@users.noreply.github.com>
2025-07-15 10:16:44 -0600

    Update Precampo.py

--- Precampo.py
+++ Precampo.py
@@ -71,26 +71,23 @@ def Precampo(usuario,puesto):
   placeholder16_3= st.empty()
   estado_3= placeholder16_3.selectbox("Estado" , options=("En Proceso", "Finalizado"),key="estado_3")
   
-  #placeholder17_3= st.empty()
-  area_3= placeholder17_3.number_input("Area en m²",min_value=0.0,key="area_3")#
+  placeholder17_3= st.empty()
+  produccion_3= placeholder17_3.number_input("Cantidad Total de Predios Producidos",min_value=0,key="produccion_3")
 
   placeholder18_3= st.empty()
-  produccion_3= placeholder18_3.number_input("Cantidad Total de Predios Producidos",min_value=0,key="produccion_3")
-
+  informales_3= placeholder18_3.number_input("Cantidad de Predios Informales",min_value=0,key="informales_3")
+  
   placeholder19_3= st.empty()
-  informales_3= placeholder19_3.number_input("Cantidad de Predios Informales",min_value=0,key="informales_3")
+  efes_3= placeholder19_3.number_input("Cantidad de F",min_value=0,key="efes_3")
   
   placeholder20_3= st.empty()
-  efes_3= placeholder20_3.number_input("Cantidad de F",min_value=0,key="efes_3")
-  
+  horas_3= placeholder20_3.number_input("Cantidad de Horas Trabajadas en el Proceso",min_value=0.0,key="horas_3")
+
   placeholder21_3= st.empty()
-  horas_3= placeholder21_3.number_input("Cantidad de Horas Trabajadas en el Proceso",min_value=0.0,key="horas_3")
+  observaciones_3= placeholder21_3.text_input("Observaciones", max_chars=60, key="observaciones_3")
 
-  placeholder22_3= st.empty()
-  observaciones_3= placeholder22_3.text_input("Observaciones", max_chars=60, key="observaciones_3")
-
-  placeholder23_3 = st.empty()
-  reporte_3 = placeholder23_3.button("Generar Reporte",key="reporte_3")
+  placeholder22_3 = st.empty()
+  reporte_3 = placeholder22_3.button("Generar Reporte",key="reporte_3")
 
   # ----- Procesos ---- #
     
@@ -117,7 +114,6 @@ def Precampo(usuario,puesto):
     placeholder20_3.empty()
     placeholder21_3.empty()
     placeholder22_3.empty()
-    placeholder23_3.empty()
     st.session_state.Procesos=False
     st.session_state.Precampo=False
 
@@ -161,7 +157,6 @@ def Precampo(usuario,puesto):
     placeholder20_3.empty()
     placeholder21_3.empty()
     placeholder22_3.empty()
-    placeholder23_3.empty()
     st.session_state.Precampo=False
     st.session_state.Historial=True
     Historial.Historial(usuario,puesto)   
@@ -191,7 +186,6 @@ def Precampo(usuario,puesto):
     placeholder20_3.empty()
     placeholder21_3.empty()
     placeholder22_3.empty()
-    placeholder23_3.empty()
     st.session_state.Precampo=False
     st.session_state.Capacitacion=True
     Capacitacion.Capacitacion(usuario,puesto)
@@ -221,7 +215,6 @@ def Precampo(usuario,puesto):
     placeholder20_3.empty()
     placeholder21_3.empty()
     placeholder22_3.empty()
-    placeholder23_3.empty()
     st.session_state.Precampo=False
     st.session_state.Otros_Registros=True
     Otros_Registros.Otros_Registros(usuario,puesto)
@@ -251,7 +244,6 @@ def Precampo(usuario,puesto):
     placeholder20_3.empty()
     placeholder21_3.empty()
     placeholder22_3.empty()
-    placeholder23_3.empty()
     st.session_state.Precampo=False
     st.session_state.Bonos_Extras=True
     Bonos_Extras.Bonos_Extras(usuario,puesto)    
@@ -281,7 +273,6 @@ def Precampo(usuario,puesto):
     placeholder20_3.empty()
     placeholder21_3.empty()
     placeholder22_3.empty()
-    placeholder23_3.empty()
     st.session_state.Ingreso = False
     st.session_state.Precampo=False
     st.session_state.Salir=True
@@ -319,8 +310,8 @@ def Precampo(usuario,puesto):
       # ----- Fin del script ---- #
     
     horas_bi = float(horas_3)
-    area_bi = float(area_3)
+
     
-    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi,operador_cc)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Precampo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','0','0','{horas_3}','{uit_3}','{hito_3}','{lote_3}','{estado_3}','{area_3}','{efes_3}','{informales_3}','P0','0','0','{observaciones_3}','{zona_3}','N/A','{horas_bi}','{area_bi}','N/A')")
+    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi,operador_cc)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Precampo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','0','0','{horas_3}','{uit_3}','{hito_3}','{lote_3}','{estado_3}','0.0','{efes_3}','{informales_3}','P0','0','0','{observaciones_3}','{zona_3}','N/A','{horas_bi}','0,0','N/A')")
     con.commit()                                                                                                                                 
     st.success('Reporte enviado correctamente')
