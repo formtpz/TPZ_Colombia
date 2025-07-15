e6276679c248c8a6b214beeb51abff85be6268f7
formtpz <137564298+formtpz@users.noreply.github.com>
2025-07-15 10:21:13 -0600

    Update Revision_Campo.py

--- Revision_Campo.py
+++ Revision_Campo.py
@@ -54,7 +54,7 @@ def Revision_Campo(usuario,puesto):
   municipio_3= placeholder10_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
 
   placeholder11_3= st.empty()
-  consecutivo_3= placeholder11_3.text_input("Número de Paquete",max_chars=10,key="consecutivo_3")
+  consecutivo_3= placeholder11_3.text_input("Número de Paquete",max_chars=15,key="consecutivo_3")
 
   placeholder12_3= st.empty()
   tipo_3= placeholder12_3.selectbox("Tipo", options=("Inspección","Primera Reinspección","Segunda Reinspección","Reproceso Inspección","Reproceso Primera Reinspección"), key="tipo_3")
