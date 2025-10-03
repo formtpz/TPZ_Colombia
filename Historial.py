ba20d7e3028dcaa5071c3c37a30f1629726911cc
formtpz <137564298+formtpz@users.noreply.github.com>
2025-10-03 13:05:30 -0600

    Update Historial.py

--- Historial.py
+++ Historial.py
@@ -328,7 +328,7 @@ def Historial(usuario,puesto):
 
       #------Creando el dataframe de Resumen calidad 
 
-      data_5_r = data_1_r.groupby(["operador_cc", "fecha"], as_index=False)[["horas","produccion","aprobados","rechazados"]].agg(np.sum)
+      data_5_r = data_1_r.groupby(["operador_cc", "semana"], as_index=False)[["horas","produccion","aprobados","rechazados"]].agg(np.sum)
       data_5_r["porcentaje_aprobacion"] = (data_5_r["aprobados"] / data_2_r["produccion"])*100
             
       placeholder25_2_7 = st.empty()
