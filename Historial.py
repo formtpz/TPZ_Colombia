b44c89e2594f0fceec24c4cb58d204c5df2da016
formtpz <137564298+formtpz@users.noreply.github.com>
2025-10-10 12:23:18 -0600

    Update Historial.py

--- Historial.py
+++ Historial.py
@@ -328,9 +328,9 @@ def Historial(usuario,puesto):
       error_producción= placeholder22_2_7.error('No existen correciones para mostrar')
     else:
       data_correciones = data_1_r[data_1_r["tipo"] == "Corrección de Calidad"]
-      agrupar_data_correciones = (data_correciones.groupby(["nombre", "semana", "proceso"], as_index=False)[["produccion"]].sum())
+      agrupar_data_correciones = data_correciones.groupby(["nombre", "fecha"], as_index=False)[["produccion","horas","efes","informales"]].agg(np.sum)
       agrupar_data_correciones["correcciones"]= agrupar_data_correciones["produccion"]
-      agrupar_data_correciones["correccion_bruta_hora"] = agrupar_data_correciones["produccion"]/data_2_r["horas"]
+      agrupar_data_correciones["correccion_bruta_hora"] = agrupar_data_correciones["produccion"]/agrupar_data_correcciones["horas"]
       agrupar_data_correciones["correccion_bruta_hora"] = agrupar_data_correciones["correccion_bruta_hora"].round(2)
 
       columnas_a_mostrar_c= ["nombre","fecha","correcciones","horas","correccion_bruta_hora"]
