4c4883efabe516480c6662d9affd1304785d06c2
formtpz <137564298+formtpz@users.noreply.github.com>
2026-05-21 13:51:03 -0600

    Update button labels and input fields in UI

--- Estado_UIT_Hito.py
+++ Estado_UIT_Hito.py
@@ -1,3 +1,4 @@
+
 # ----- Librerías ---- #
 
 import streamlit as st
@@ -37,42 +38,45 @@ def Estado_UIT_Hito(usuario,puesto):
   otros_registros_3 = placeholder5_3.button("Otros Registros",key="otros_registros_3")
 
   placeholder6_3 = st.sidebar.empty()
-  bonos_extras_3 = placeholder6_3.button("Bonos y Horas Extra",key="bonos_extras_3")
+  bonos_extras_3 = placeholder6_3.button("Bonos y Extras",key="bonos_extras_3")
 
   placeholder7_3 = st.sidebar.empty()
   salir_3 = placeholder7_3.button("Salir",key="salir_3")
 
   placeholder8_3 = st.empty()
-  estado_uit_hito_3 = placeholder8_3.title("Calidad Externa XTF")
+  revision_campo_3 = placeholder8_3.title("Revisión de Planos")
 
   default_date_3 = datetime.now(pytz.timezone('America/Guatemala'))
-  
+
   placeholder9_3= st.empty()
   fecha_3= placeholder9_3.date_input("Fecha",value=default_date_3,key="fecha_3")
   
   placeholder10_3= st.empty()
-  lote_3= placeholder10_3.selectbox("Lote",options=("1","2","3"), key="lote_3")
-                                  
+  municipio_3= placeholder10_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
+
   placeholder11_3= st.empty()
-  municipio_3= placeholder11_3.selectbox("Municipio", options=("Cabuyaro","Chalán","Colombia","Cuítiva","Iza","Los Palmitos","Morroa","Trinidad","San Estanislao","San Luis de Cubarral","Zambrano"), key="municipio_3")
-  
+  consecutivo_3= placeholder11_3.text_input("Número de Paquete",max_chars=15,key="consecutivo_3")
+
   placeholder12_3= st.empty()
-  hito_3= placeholder12_3.selectbox("Hito",options=("1","2","3","4","5","6","7","8","9","10"), key="hito_3") 
-  
+  tipo_3= placeholder12_3.selectbox("Tipo", options=("Inspección","Primera Reinspección","Segunda Reinspección","Tercera Reinspección","Cuarta Reinspección","Quinta Reinspección","Reproceso Inspección","Reproceso Primera Reinspección","Reproceso Segunda Reinspección","Reproceso Tercera Reinspección","Reproceso cuarta Reinspección","Control de calidad Supervisión","En conflicto"), key="tipo_3")
+
   placeholder13_3= st.empty()
-  nombre_xtf_3= placeholder13_3.text_input("Nombre XTF",max_chars=40,key="nombre_xtf_3")
-  
-  placeholder14_3= st.empty()
-  estado_3= placeholder14_3.selectbox("Zona",options=("Rural","Urbana"), key="estado_3")
+  zona_3= placeholder13_3.selectbox("zona", options=("Rural","Urbano"), key="zona_3")
+
+  #placeholder14_3= st.empty()
+  #predios_3= placeholder14_3.number_input("Cantidad de Predios",min_value=0,step=1,key="predios_3")
 
   placeholder15_3= st.empty()
-  total_de_errores_3= placeholder15_3.number_input("Total de Errores SIG",min_value=0,step=1,key="total_de_errores_3")
-  
+  aprobados_3= placeholder15_3.number_input("Cantidad de Predios Aprobados",min_value=0,step=1,key="aprobados_3")
+
   placeholder16_3= st.empty()
-  errores_por_excepciones_3= placeholder16_3.number_input("Errores por excepciones",min_value=0,step=1,key="errores_por_excepciones_3")
-   
-  placeholder17_3 = st.empty()
-  reporte_3 = placeholder17_3.button("Generar Reporte",key="reporte_3")
+  rechazados_3= placeholder16_3.number_input("Cantidad de Predios Rechazados",min_value=0,step=1,key="rechazados_3")
+  
+  placeholder17_3= st.empty()
+  horas_3= placeholder17_3.number_input("Cantidad de Horas Trabajadas en el Proceso",min_value=0.0,key="horas_3")
+
+  placeholder18_3 = st.empty()
+  reporte_3 = placeholder18_3.button("Generar Reporte",key="reporte_3")
 
   # ----- Procesos ---- #
     
@@ -90,12 +94,13 @@ def Estado_UIT_Hito(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
+    placeholder18_3.empty()
     st.session_state.Procesos=False
-    st.session_state.calidad_externa_xft=False
+    st.session_state.Revision_Campo=False
 
     perfil=pd.read_sql(f"select perfil from usuarios where usuario ='{usuario}'",uri)
     perfil= perfil.loc[0,'perfil']
@@ -111,8 +116,7 @@ def Estado_UIT_Hito(usuario,puesto):
     elif perfil=="3":  
 
       Procesos.Procesos3(usuario,puesto)       
-
-
+  
   #----- Historial ---- #
     
   elif historial_3:
@@ -129,11 +133,12 @@ def Estado_UIT_Hito(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
-    st.session_state.calidad_externa_xft=False
+    placeholder18_3.empty()
+    st.session_state.Revision_Campo=False
     st.session_state.Historial=True
     Historial.Historial(usuario,puesto)   
 
@@ -153,11 +158,12 @@ def Estado_UIT_Hito(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
-    st.session_state.calidad_externa_xft=False
+    placeholder18_3.empty()
+    st.session_state.Revision_Campo=False
     st.session_state.Capacitacion=True
     Capacitacion.Capacitacion(usuario,puesto)
 
@@ -177,15 +183,16 @@ def Estado_UIT_Hito(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
-    st.session_state.calidad_externa_xft=False
+    placeholder18_3.empty()
+    st.session_state.Revision_Campo=False
     st.session_state.Otros_Registros=True
     Otros_Registros.Otros_Registros(usuario,puesto)
 
-  # ----- Bonos y Horas Extra---- #
+  # ----- Bonos y Horas Extras ---- #
     
   elif bonos_extras_3:
     placeholder1_3.empty()
@@ -201,11 +208,12 @@ def Estado_UIT_Hito(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
-    st.session_state.calidad_externa_xft=False
+    placeholder18_3.empty()
+    st.session_state.Revision_Campo=False
     st.session_state.Bonos_Extras=True
     Bonos_Extras.Bonos_Extras(usuario,puesto)    
 
@@ -225,12 +233,13 @@ def Estado_UIT_Hito(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
+    placeholder18_3.empty()
     st.session_state.Ingreso = False
-    st.session_state.calidad_externa_xft=False
+    st.session_state.Revision_Campo=False
     st.session_state.Salir=True
     Salir.Salir()
 
@@ -246,11 +255,26 @@ def Estado_UIT_Hito(usuario,puesto):
     supervisor_3= pd.read_sql(f"select supervisor from usuarios where usuario ='{usuario}'",uri)
     supervisor_3 = supervisor_3.loc[0,'supervisor']
 
+    produccion_3 = aprobados_3 + rechazados_3
+
     semana_3 = fecha_3.isocalendar()[1]
 
     año_3 = fecha_3.isocalendar()[0]
 
+    # ----- Almacenar Lote_3 según municipio seleccionado ---- #
     
-    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi,operador_cc,total_de_errores,errores_por_excepciones,tipo_de_errores,conteo_de_errores)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Calidad Externa XTF','{fecha_3}','{semana_3}','{año_3}','{municipio_3}','N/A','0','0','0','0.0','{nombre_xtf_3}','{hito_3}','{lote_3}','{estado_3}','0.0','0','0','N/A','0','0','N/A','N/A','N/A','0','0','N/A','{total_de_errores_3}','{errores_por_excepciones_3}','N/A','0')")
+    lote_3_municipios = {"Cabuyaro", "Colombia", "San Luis de Cubarral"}
+    lote_2_municipios = {"Trinidad", "Iza", "Cuítiva"}
+   
+    if municipio_3 in lote_3_municipios:
+      lote_3 = '3'
+    elif municipio_3 in lote_2_municipios:
+      lote_3 = '2'
+    else:
+      lote_3 = '1'
+      # ----- Fin del script ---- #
+    unidad_3=municipio_3
+    horas_bi = float(horas_3)
+    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi,operador_cc,total_de_errores,errores_por_excepciones,tipo_de_errores,conteo_de_errores)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Revisión de Campo','{fecha_3}','{semana_3}','{año_3}','{unidad_3}','{tipo_3}','{produccion_3}','{aprobados_3}','{rechazados_3}','{horas_3}','UIT-0','0','{lote_3}','N/A','0.0','0','0','{consecutivo_3}','0','0','N/A','{zona_3}','N/A','{horas_bi}','0','N/A','0','0','N/A','0')")
     con.commit()                                                                                                                                 
     st.success('Reporte enviado correctamente')
